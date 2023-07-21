5539c86dc1adddd11f133ff637eb1731eb5f6dac
Deathdrummer <deathdrumer@yandex.ru>
2023-07-21 17:34:17 +0300

    fixes 1 --D

--- resources/views/site/section/orders/render/item.blade.php
+++ resources/views/site/section/orders/render/item.blade.php
@@ -82,12 +82,12 @@
 	<div class="col-auto">
 		<div class="order__block order__block-noborder">
 			<x-buttons-group class="order__right" size="small">
-				@if(!isset($status) || $status == App\Enums\OrderStatus::new)
+				@if(!isset($status) || $status == App\Enums\OrderStatus::fromKey('new'))
 					<x-button w="50px" action="toCancelListBtn:{{$id ?? null}}" variant="red"><i class="fa-solid fa-fw fa-ban"></i></x-button>
 					<x-button w="50px" action="toWaitListBtn:{{$id ?? null}}" variant="blue"><i class="fa-solid fa-fw fa-hourglass-half"></i></x-button>
-				@elseif($status == App\Enums\OrderStatus::wait)
+				@elseif($status == App\Enums\OrderStatus::fromKey('wait'))
 					<x-button w="50px" action="toCancelListBtn:{{$id ?? null}}" variant="red"><i class="fa-solid fa-fw fa-ban"></i></x-button>
-				@elseif($status == App\Enums\OrderStatus::cancel)
+				@elseif($status == App\Enums\OrderStatus::fromKey('cancel'))
 					
 				@endif
 				<x-button w="50px" variant="green" action="toTimesheetBtn:{{$id ?? null}},{{$date ?? null}},{{$order ?? '-'}}"><i class="fa-solid fa-fw fa-angles-right"></i></x-button>
