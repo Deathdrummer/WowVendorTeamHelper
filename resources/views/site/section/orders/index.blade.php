f222eb97cbe206bd1771c6f73fda6bec9894714f
Deathdrummer <deathdrumer@yandex.ru>
2023-09-22 12:32:43 +0300

    Добавил рингтон финал --D

--- resources/views/site/section/orders/index.blade.php
+++ resources/views/site/section/orders/index.blade.php
@@ -172,7 +172,6 @@
 	
 	$.getOrdersAction = async (selector, isActive, stat) => {
 		if (isActive) return;
-		ringtone('notify.mp3');
 		$('#ordersTypesChuser').setAttrib('disabled');
 		status.value = stat;
 		currentPage.value = 1;
