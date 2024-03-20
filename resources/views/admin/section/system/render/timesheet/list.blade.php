28ad55850da10c6a8d99b5ada5221a27598385dc
Deathdrummer <deathdrumer@yandex.ru>
2024-03-20 16:40:56 +0300

    настройки пользователя: отобразить прошедшие заказы в актуальных фикс 4 --D

--- resources/views/admin/section/system/render/timesheet/list.blade.php
+++ resources/views/admin/section/system/render/timesheet/list.blade.php
@@ -1,7 +1,9 @@
 {{-- <x-data :data="$data"> --}}
 @if($list)
 	@foreach ($list as $item)
-		@if($listType == 'actual' && ((($item['isPast'] ?? false) && $showPastOrdersInActual) || !$item['isPast']))
+		@if($listType == 'past')
+			@include($itemView, $item)
+		@elseif($listType == 'actual' && ((($item['isPast'] ?? false) && $showPastOrdersInActual) || !$item['isPast']))
 			@include($itemView, $item)
 		@endif
 	@endforeach
